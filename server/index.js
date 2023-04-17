2dfe82154895f9b45e70e2c96e11f7fe5329a3a5
Sterberino <91395511+Sterberino@users.noreply.github.com>
2023-04-17 15:51:00 -0700

    Changed the start scripts so that everything runs on one port. Frontend is now served by express backend

--- server/index.js
+++ server/index.js
@@ -3,12 +3,27 @@ const request = require('request');
 const ConnectDB = require('./db/connect.js');
 const errorHandlerMiddleware = require('./middleware/error-handler.js');
 const authenticationMiddleware = require('./middleware/authentication.js');
-
+const xss = require('xss-clean');
+const helmet = require('helmet');
+const cors = require('cors')
+const rateLimiter = require('express-rate-limit')
 require('express-async-errors');
 require('dotenv').config();
+const path = require('node:path');
 
 const app = express();
 app.use(express.json());
+
+app.use(xss());
+app.use(cors())
+app.use(helmet());
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 5 * 60 * 1000, // 5 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 
 //Routers
 const ExerciseDiaryRouter = require('./routes/ExerciseDiary.js');
@@ -29,7 +44,13 @@ app.use('/api/v1/waterDiary', authenticationMiddleware, WaterDiaryRouter)
 //Middleware
 app.use(errorHandlerMiddleware);
 
-const port = 3001;
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "../build")));
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../", "build", "index.html"));
+});
+
 
 const Start = async ()=>{
     try{
